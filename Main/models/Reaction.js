818913170cf8ff307dbdc6677c4caa0a3203ca84
Kimberly Robinson <158204628+kimberlyrobinson11122@users.noreply.github.com>
2024-07-14 21:21:44 -0700

    added Reaction.js was originally missing

---
+++ Main/models/Reaction.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Define the Reaction schema
+const ReactionSchema = new Schema({
+  reactionId: {
+    type: mongoose.Schema.Types.ObjectId,
+    default: () => new mongoose.Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = ReactionSchema;
+
